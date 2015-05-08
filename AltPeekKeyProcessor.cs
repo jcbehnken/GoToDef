a86a21e3073c4a5291039262290a4c1f51c079a8
jcbehnken <jcbehnken@users.noreply.github.com>
2015-05-08 18:06:07 -0400

    Broke some files apart to make refactoring a bit easier.

---
+++ AltPeekKeyProcessor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio;
+
+namespace GoToDef
+{
+
+
+    /// <summary>
+    /// Listen for the alt key being pressed or released to update the AltKeyStateChanged for a view.
+    /// </summary>
+    internal sealed class AltPeekKeyProcessor : KeyProcessor
+    {
+        AltKeyState _state;
+
+        public AltPeekKeyProcessor(AltKeyState state)
+        {
+            _state = state;
+        }
+
+        void UpdateState(KeyEventArgs args)
+        {
+            _state.Enabled = (args.KeyboardDevice.Modifiers & ModifierKeys.Alt) != 0 &&
+                             (args.KeyboardDevice.Modifiers & ModifierKeys.Shift) == 0;
+        }
+
+        public override void PreviewKeyDown(KeyEventArgs args)
+        {
+            UpdateState(args);
+        }
+
+        public override void PreviewKeyUp(KeyEventArgs args)
+        {
+            UpdateState(args);
+        }
+    }
+}
---
+++ AltPeekMouseHandlerProvider.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Windows;
+using System.Windows.Input;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Operations;
+using Microsoft.VisualStudio.Utilities;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio;
+
+namespace GoToDef
+{
+
+    [Export(typeof(IMouseProcessorProvider))]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    [ContentType("code")]
+    [Name("AltPeek")]
+    [Order(Before = "GoToDefMouseHandlerProvider")]
+    internal sealed class AltPeekMouseHandlerProvider : IMouseProcessorProvider
+    {
+        [Import]
+        IClassifierAggregatorService AggregatorFactory = null;
+
+        [Import]
+        ITextStructureNavigatorSelectorService NavigatorService = null;
+
+        [Import]
+        SVsServiceProvider GlobalServiceProvider = null;
+
+        public IMouseProcessor GetAssociatedProcessor(IWpfTextView view)
+        {
+            var buffer = view.TextBuffer;
+
+            IOleCommandTarget shellCommandDispatcher = GetShellCommandDispatcher(view);
+
+            if (shellCommandDispatcher == null)
+                return null;
+
+            return new AltPeekMouseHandler(view,
+                                           shellCommandDispatcher,
+                                           AggregatorFactory.GetClassifier(buffer),
+                                           NavigatorService.GetTextStructureNavigator(buffer),
+                                           AltKeyState.GetStateForView(view));
+        }
+
+        #region Private helpers
+
+        /// <summary>
+        /// Get the SUIHostCommandDispatcher from the global service provider.
+        /// </summary>
+        IOleCommandTarget GetShellCommandDispatcher(ITextView view)
+        {
+            return GlobalServiceProvider.GetService(typeof(SUIHostCommandDispatcher)) as IOleCommandTarget;
+        }
+
+        #endregion
+    }
+}
